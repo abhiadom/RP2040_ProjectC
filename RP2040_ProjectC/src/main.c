96f83d4eb874294e97926d220843463c0c9ed1d7
abhiadom <abhi@adom.inc>
2024-07-29 10:36:34 -0500

    Update main.c

--- RP2040_ProjectC/src/main.c
+++ RP2040_ProjectC/src/main.c
@@ -1,7 +1,25 @@
-#include "utils.h"
+#include <stdio.h>
+#include "pico/stdlib.h"
 
 int main() {
-    printf("Hello, RP2040!\n");
-    print_utils();
-    return 0;
+
+    const uint led_pin = 25;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    // Loop forever
+    while (true) {
+
+        // Blink LED
+        printf("Blinking!\r\n");
+        gpio_put(led_pin, true);
+        sleep_ms(1000);
+        gpio_put(led_pin, false);
+        sleep_ms(1000);
+    }
 }
